8272849e61e360c489cc493824426a2b44179d8e
mrlucretius <alex@rettc.com>
2020-01-05 17:50:51 -0700

    Initial Commit
    Initial Commit

---
+++ FulcrumBot/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FulcrumBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static async Task GetAccountBalance()
+        {
+            // this is my 
+            var web3 = new Web3("https://mainnet.infura.io/v3/88b95d93ad104bf0ad6a881056e40990");
+
+            var balance = await web3.Eth.GetBalance.SendRequestAsync("0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae");
+            Console.WriteLine($"Balance in Wei: {balance.Value}");
+
+            var etherAmount = Web3.Convert.FromWei(balance.Value);
+            Console.WriteLine($"Balance in Ether: {etherAmount}");
+        }
+    }
+}
